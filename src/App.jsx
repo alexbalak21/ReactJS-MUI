ba1a3fc2e608baed3801346356cd7bdaf3259347
alexbalak21 <alex.balak@outlook.com>
2023-09-27 14:24:07 +0200

    Working MUI implementation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Typography } from "@mui/material"
+
+function App() {
+    return (
+        <div>
+            <Typography variant="h3">Hello World !</Typography>
+        </div>
+    )
+}
+
+export default App
